f59aa474b8e68dd953f4df584d5ecc2efa4d59d4
BartoszBartczak <intermarism@gmail.com>
2022-11-24 10:51:59 +0100

    adding posts preparation

---
+++ src/app/posts.ts
@@ -0,0 +1,6 @@
+export interface Post {
+    id: number;
+    pictureUrl: string;
+    title: string;
+    text: string;
+}
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
